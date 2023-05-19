dca64c66e9b196b7de4c2ee9f7ef8c8df654d1bd
christianmoraless <christiandmoraless@gmail.com>
2023-05-19 02:09:32 -0400

    creating conection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,3 @@
-import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -10,10 +9,10 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: +process.env.PORT,
-      database: process.env.DB_NAME,
+      port: +process.env.DB_HOST,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
